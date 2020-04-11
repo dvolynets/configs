58759a2eee0b961bdd09239952233684b2f67ebd
DimaVolynets450501 <dimanvolina@gmail.com>
2020-04-11 14:11:26 +0000

    removed old configs

--- emacs.d/elpa/deferred-20170901.630/deferred-autoloads.el
+++
@@ -1,15 +0,0 @@
-;;; deferred-autoloads.el --- automatically extracted autoloads
-;;
-;;; Code:
-(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
-
-;;;### (autoloads nil nil ("deferred.el") (23032 31641 664900 224000))
-
-;;;***
-
-;; Local Variables:
-;; version-control: never
-;; no-byte-compile: t
-;; no-update-autoloads: t
-;; End:
-;;; deferred-autoloads.el ends here
--- setup-ggtags.el
+++
@@ -1,3 +0,0 @@
-(require 'ggtags)
-
-(define-key ggtags-mode-map (kbd "C-c C-f") 'ggtags-find-file)
